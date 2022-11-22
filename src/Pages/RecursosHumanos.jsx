b87325dc6780c39052e7dcfd945cf007889e0cf2
SantiG95 <santiguerra86@gmail.com>
2022-11-22 20:36:59 -0300

    Avanzada la pagina de recursos humanos

--- src/Pages/RecursosHumanos.jsx
+++ src/Pages/RecursosHumanos.jsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
-import styles from "./../Styles/RecursosHumanos/Workers.css";
+import styles from "./../Styles/RecursosHumanos/RecursosHumanos.css";
+import buttonStyle from "./../Styles/RecursosHumanos/Botones.css";
+
+import trabajadores from "./../Img/personas_icon.png";
+import reportes from "./../Img/reportes_icon.png";
 
 
 function RecursosHumanos() {
@@ -11,32 +15,45 @@ function RecursosHumanos() {
 
   return (
     <div className="body">
-      <p className="trabajadores-separator">Trabajadores</p>
-      <div className="workers-list">
-        <div className="header-list">
-          <div><p>Nombre</p></div>
-          <div><p>Apellido</p></div>
-          <div><p>Legajo</p></div>
+      <div className="recursos-humanos-container trabajadores">
+        <p className="section-title">Trabajadores</p>
+        <img className="recursos-icon" src={trabajadores} alt="" />
+        <div className="workers-list">
+          <div className="header-list">
+            <div><p>Nombre</p></div>
+            <div><p>Apellido</p></div>
+            <div><p>Legajo</p></div>
+          </div>
+          <div className="list-element">
+            <div><p>Mario</p></div>
+            <div><p>Mendoza</p></div>
+            <div><p>1</p></div>
+          </div>
+          <div className="list-element">
+            <div><p>Maria</p></div>
+            <div><p>Perez</p></div>
+            <div><p>2</p></div>
+          </div>
+          <div className="list-element">
+            <div><p>Patricio</p></div>
+            <div><p>Gaona</p></div>
+            <div><p>3</p></div>
+          </div>
+          <div className="list-element" onClick={verTareas}>
+            <div><p>Juan</p></div>
+            <div><p>Gonzalez</p></div>
+            <div><p>4</p></div>
+          </div>
         </div>
-        <div className="list-element">
-          <div><p>Mario</p></div>
-          <div><p>Mendoza</p></div>
-          <div><p>1</p></div>
+      </div>
+      <div className="recursos-humanos-container reportes">
+        <p className="section-title">Reportes</p>
+        <img className="recursos-icon" src={reportes} alt="" />
+        <div className="recursos-button">
+          <p>Generar reporte de horas trabajadas de un trabajador</p>
         </div>
-        <div className="list-element">
-          <div><p>Maria</p></div>
-          <div><p>Perez</p></div>
-          <div><p>2</p></div>
-        </div>
-        <div className="list-element">
-          <div><p>Patricio</p></div>
-          <div><p>Gaona</p></div>
-          <div><p>3</p></div>
-        </div>
-        <div className="list-element" onClick={verTareas}>
-          <div><p>Juan</p></div>
-          <div><p>Gonzalez</p></div>
-          <div><p>4</p></div>
+        <div className="recursos-button">
+          <p>Generar reporte de horas trabajadas por proyecto</p>
         </div>
       </div>
     </div>
